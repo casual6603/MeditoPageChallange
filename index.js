976ca6cb91bae0c3df2eeb5a889a838eac1857f6
casual6603 <135954286+casual6603@users.noreply.github.com>
2024-01-18 14:46:43 -0800

    please

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ $('.placeholder').on('click', function (ev) {
   $('.list__ul').toggle();
 });
 
- $('.list__ul a').on('click', function (ev) {
-   ev.preventDefault();
-   var index = $(this).parent().index();
-   
-   $('.placeholder').text( $(this).text() ).css('opacity', '1');
-   
-   console.log($('.list__ul').find('li').eq(index).html());
-   
-   $('.list__ul').find('li').eq(index).prependTo('.list__ul');
-   $('.list__ul').toggle();   
-   
- });
+$('.list__ul a').on('click', function (ev) {
+  ev.preventDefault();
+  var index = $(this).parent().index(); 
+  $('.placeholder').text( $(this).text() ).css('opacity', '1');
+  
+  console.log($('.list__ul').find('li').eq(index).html());
+  
+  $('.list__ul').find('li').eq(index).prependTo('.list__ul');
+  $('.list__ul').toggle();     
+});
 
 
 $('select').on('change', function (e) {
